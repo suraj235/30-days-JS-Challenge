e88e7961f0bf9cd2e3442e9987849a6ce0a8666d
suraj235 <suraj@krenovate.com>
2024-07-17 01:02:19 +0530

    Day 1 Challenge Completed!

---
+++ Day1/index.js
@@ -0,0 +1,57 @@
+/**
+ * Activity 1: Variable Declaration.
+ */
+
+// Task1
+var var1 = 23;
+console.log(var1);
+
+// Task2
+let var2 = 'ChaiCode';
+console.log(var2);
+
+
+/**
+ * Activity 2: Constant Declaration.
+ */
+
+// Task3
+const var3 = false;
+console.log(var3);
+
+
+/**
+ * Activity 3: Data Types.
+ */
+let number = 3;
+let string = 'ChaiCode';
+let isActive = true;
+let myArray = [2, 3, 5, 'Suraj'];
+let obj = {
+    name: 'suraj',
+    age: 26,
+    profession: 'Software Developer'
+};
+
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof isActive)
+console.log(typeof myArray)
+console.log(typeof obj)
+
+
+/**
+ * Activity 4: Reassigning variables.
+ */
+let var4 = 'Coding!'
+console.log(var4)
+var4 = 'Happy Coding!!'
+console.log(var4)
+
+
+/**
+ * Activity 4: Understanding const.
+ */
+const var5 = 'Day1';
+var5 = 'Day1 Challenge Completed';
+// We can't reassign a value to the variable declared with const data type but can do with var & let.
